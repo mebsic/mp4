7305f547861fa2f346d475445e58e5e0596baf78
mebsic <tonydeleanu@yahoo.ca>
2021-09-28 16:01:22 -0400

    Update app

--- app.js
+++ app.js
@@ -13,8 +13,16 @@ app.get('/favicon.ico', (response) => {
     response.sendFile(path.resolve(__dirname + '/favicon.ico'));
 });
 
-app.get("/", (response) => {
-    response.sendFile(__dirname);
+app.get("/", (req, res) => {
+    res.sendFile(__dirname);
+});
+
+app.get("/terms", (req, res) => {
+    res.sendFile(path.resolve(__dirname + "/terms.html"));
+});
+
+app.get("*", (req, res) => {
+    res.status(404).sendFile((path.join(__dirname + "/404.html")));
 });
 
 app.get('/download', (req, res) => {
@@ -43,10 +51,6 @@ app.get('/download', (req, res) => {
         res.header("Content-Disposition", 'attachment; filename="video.mp4');
         ffmpegProcess.stdio[5].pipe(res);
     });
-});
-
-app.get("*", (req, res) => {
-    res.status(404).sendFile((path.join(__dirname + "/404.html")));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
